c650f7ac5f74099248eb45832bc72451c62169b4
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2025-05-28 15:03:56 +0100

    try interal url

--- db/Pool.js
+++ db/Pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg")
 require('dotenv').config();
 
-const connectionString = process.env.NODE_ENV === "production" ? process.env.EXTERNAL_DB_URL : process.env.LOCAL_DB_URL
+const connectionString = process.env.NODE_ENV === "production" ? process.env.INTERNAL_DB_URL : process.env.LOCAL_DB_URL
 
 const dbPool = new Pool({
     connectionString
