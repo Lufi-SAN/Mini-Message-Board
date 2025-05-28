ee9949585477ee027e12acb5f5181b0868f1e5e5
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2025-05-28 15:11:30 +0100

    Add ssl

--- db/Pool.js
+++ db/Pool.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const connectionString = process.env.NODE_ENV === "production" ? process.env.INTERNAL_DB_URL : process.env.LOCAL_DB_URL
 
 const dbPool = new Pool({
-    connectionString
+    connectionString,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
 })
 
 module.exports = dbPool
